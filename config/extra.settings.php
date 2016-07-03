5ae6921c29fd39cee7638622fd2220b3e3ab4440
Karel Bemelmans <karel.bemelmans@nordcloud.se>
2016-07-03 23:25:09 +0200

    Added more stuff to extra.settings.php

--- config/extra.settings.php
+++ config/extra.settings.php
@@ -1,6 +1,15 @@
 <?php
 
-// The content of this file gets
+// The content of this file gets included at the end of settings.php
+//
+// Remember: This file is built into your container, so if you make changes here
+// you need to rebuild your Drupal container!
 
+// This is just a test, clean urls are not needed to be disabled.
 $conf['clean_url'] = 0;
+
+// Set the private files url to the VOLUME we defined
+$conf['file_private_path'] = '/data/private';
+
+// More things can be added here.
 $conf['foo'] = 'bar';
